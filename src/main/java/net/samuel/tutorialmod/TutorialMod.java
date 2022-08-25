c3449785fc98f33a61311f686a17e35cdd19d6ab
SamAcosta02 <93208126+SamAcosta02@users.noreply.github.com>
2022-08-25 12:25:02 -0500

    Added an important comment

--- src/main/java/net/samuel/tutorialmod/TutorialMod.java
+++ src/main/java/net/samuel/tutorialmod/TutorialMod.java
@@ -16,7 +16,7 @@ public class TutorialMod {
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-
+    // Very Importat Comment
     public TutorialMod () {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
